99bc3fe0832e63d64292426110b23acd47969093
Fardins <66249317+Fardins@users.noreply.github.com>
2024-12-17 00:25:01 +0600

    new added predict.py and train.py

---
+++ predict.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+import cv2
+import numpy as np  # Required for handling numpy arrays
+
+model_path = './runs/segment/train/weights/last.pt'
+image_path = './data/images/val/748.jpg'
+
+img = cv2.imread(image_path)
+H, W, _ = img.shape
+
+model = YOLO(model_path)
+
+results = model(img)
+
+for result in results:
+    for j, mask in enumerate(result.masks.data):
+
+        mask = mask.numpy() * 255
+
+        mask = cv2.resize(mask, (W, H))
+
+        cv2.imwrite('./output.png', mask)
